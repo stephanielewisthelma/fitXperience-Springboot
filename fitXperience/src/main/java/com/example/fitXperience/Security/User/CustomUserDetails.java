f979eae45d55a6982c4e2eab2c3743dd90d2323a
stephanielewisthelma <slewisidc230004@gmail.com>
2025-06-23 14:04:06 +0100

    application start up

---
+++ fitXperience/src/main/java/com/example/fitXperience/Security/User/CustomUserDetails.java
@@ -0,0 +1,68 @@
+package com.example.fitXperience.Security.User;
+
+
+import com.example.fitXperience.Model.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+@RequiredArgsConstructor
+public class CustomUserDetails implements UserDetails {
+    private final User user;
+
+    public CustomUserDetails(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Set<SimpleGrantedAuthority> authorities = new HashSet<>();
+
+//        map each role name
+        user.getRole().forEach(roles ->
+                authorities.add(new SimpleGrantedAuthority(role.getName())));
+
+//      map each permission name (e.g MANAGE_USERS)
+        user.getRole().stream()
+                .flatMap(role -> role.getPermissions().stream())
+                .map(perm -> new SimpleGrantedAuthority(perm.getName()))
+                .forEach(authorities::add);
+
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return user.getStatus() != UserStatus.LOCKED;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return user.getStatus() == UserStatus.ACTIVE;
+    }
+}
---
+++ fitXperience/src/main/java/com/example/fitXperience/Security/User/CustomUserDetailsService.java
@@ -0,0 +1,27 @@
+package com.example.fitXperience.Security.User;
+
+
+import com.example.fitXperience.Repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username)
+                .map(CustomUserDetails::new)
+                .orElseThrow(() -> new UsernameNotFoundException("User with email: "+ username + "does not exist."));
+    }
+}
